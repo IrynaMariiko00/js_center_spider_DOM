f923d716b10057c007c2ef530a6b4b55207c9a96
Iryna Mariiko <IrynaMM497@gmail.com>
2024-10-26 19:55:30 +0300

    make build

---
+++ index.658a2fd8.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=document.querySelector(".wall"),o=t.offsetWidth,s=t.offsetHeight,f=e.offsetWidth,l=e.offsetHeight;t.style.position="absolute",t.style.left="".concat((f-o)/2,"px"),t.style.top="".concat((l-s)/2,"px");
+//# sourceMappingURL=index.658a2fd8.js.map
